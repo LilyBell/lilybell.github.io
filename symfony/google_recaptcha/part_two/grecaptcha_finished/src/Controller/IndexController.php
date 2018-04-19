e2ea6a7ab8eb95b0a63757007f8c021ae4e8d50c
LilyBell <aurelian.womack@gmail.com>
2018-04-19 12:55:19 -0400

    added info alert

---
+++ symfony/google_recaptcha/part_two/grecaptcha_finished/src/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lilybell
+ * Date: 4/7/18
+ * Time: 8:10 PM
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return $this->render()
+    }
+}
